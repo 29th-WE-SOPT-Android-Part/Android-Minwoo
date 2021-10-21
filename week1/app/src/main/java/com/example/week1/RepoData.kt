c2e770cf8e2ddb45a42dcfc47089545f3e55b34a
minwoo <alsd20177@gmail.com>
2021-10-21 19:57:41 +0900

    make data class & each ViewHolder, Adapter

---
+++ week1/app/src/main/java/com/example/week1/RepoData.kt
@@ -0,0 +1,6 @@
+package com.example.week1
+
+data class RepoData(
+    val repo : String,
+    val repoDetail : String
+)
--- week1/app/src/main/java/com/example/week1/RepoFragment.kt
+++ week1/app/src/main/java/com/example/week1/RepoFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.week1.databinding.FragmentRepoBinding
 
 class RepoFragment : Fragment() {
+
+    private var _binding: FragmentRepoBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_repo, container, false)
+
+        _binding = FragmentRepoBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 }
