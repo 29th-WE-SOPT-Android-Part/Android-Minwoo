a06a9fc52bdb03c26e912ef6d6ac6c713eca4d9b
minwoo <alsd20177@gmail.com>
2021-12-17 04:09:12 +0900

    #5 [FEAT] : make AutoLoginSharedPreferences object

---
+++ week1/app/src/main/java/com/example/week1/util/AutoLoginSharedPreferences.kt
@@ -0,0 +1,36 @@
+package com.example.week1.util
+
+import android.content.Context
+
+object AutoLoginSharedPreferences {
+    private const val STORAGE_KEY = "USER_AUTH"
+    private const val AUTO_LOGIN = "AUTO_LOGIN"
+
+    fun getAutoLogin(context: Context): Boolean {
+        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
+        return preferences.getBoolean(AUTO_LOGIN, false)
+    }
+
+    fun setAutoLogin(context: Context, value: Boolean) {
+        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
+        preferences.edit()
+            .putBoolean(AUTO_LOGIN,value)
+            .apply()
+    }
+
+    fun removeAutoLogin(context: Context) {
+        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
+        preferences.edit()
+            .remove(AUTO_LOGIN)
+            .apply()
+    }
+
+    fun clearAutoLogin(context: Context) {
+        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
+        preferences.edit()
+            .clear()
+            .apply()
+    }
+
+
+}
