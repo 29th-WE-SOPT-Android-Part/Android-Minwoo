3c3a50e7bf881ebcc705eb0732727ef9b51f9bbe
minwoo <alsd20177@gmail.com>
2021-12-17 04:15:15 +0900

    #5 [FEAT] : edit AutoLoginSharedPreferences object

--- week1/app/src/main/java/com/example/week1/util/AutoLoginSharedPreferences.kt
+++ week1/app/src/main/java/com/example/week1/util/AutoLoginSharedPreferences.kt
@@ -1,36 +1,35 @@
 package com.example.week1.util
 
 import android.content.Context
+import android.content.SharedPreferences
 
 object AutoLoginSharedPreferences {
     private const val STORAGE_KEY = "USER_AUTH"
     private const val AUTO_LOGIN = "AUTO_LOGIN"
 
-    fun getAutoLogin(context: Context): Boolean {
-        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
-        return preferences.getBoolean(AUTO_LOGIN, false)
+    fun getAutoLoginSharedPreferences(context: Context): SharedPreferences {
+        return context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
     }
 
-    fun setAutoLogin(context: Context, value: Boolean) {
-        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
-        preferences.edit()
-            .putBoolean(AUTO_LOGIN,value)
-            .apply()
-    }
+    fun getAutoLogin(context: Context): Boolean = getAutoLoginSharedPreferences(context).getBoolean(
+        AUTO_LOGIN, false
+    )
 
-    fun removeAutoLogin(context: Context) {
-        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
-        preferences.edit()
-            .remove(AUTO_LOGIN)
-            .apply()
-    }
 
-    fun clearAutoLogin(context: Context) {
-        val preferences = context.getSharedPreferences(STORAGE_KEY, Context.MODE_PRIVATE)
-        preferences.edit()
-            .clear()
-            .apply()
-    }
+    fun setAutoLogin(context: Context, value: Boolean) = getAutoLoginSharedPreferences(context)
+        .edit()
+        .putBoolean(AUTO_LOGIN, value)
 
+
+    fun removeAutoLogin(context: Context) = getAutoLoginSharedPreferences(context)
+        .edit()
+        .remove(AUTO_LOGIN)
+        .apply()
+
+
+    fun clearAutoLogin(context: Context) = getAutoLoginSharedPreferences(context)
+        .edit()
+        .clear()
+        .apply()
 
 }
